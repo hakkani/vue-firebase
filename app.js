f9da79bb75d44ffc7d4d6b578eeaacd734dbc15b
Hakkani <refathasan5124@gmail.com>
2020-08-29 11:52:47 +0900

    15. Looping with v-for

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ new Vue({
   el: "#app",
   data: {
     title: "Becoming a Vue ninja",
-    showName: true,
-    showAge: true,
+    items: ["Mushroom", "Green Shells", "Red Shells", "Banana", "Star"],
+    ninjas: [
+      { name: "Crystal", age: 25, belt: "Black" },
+      { name: "Ryu", age: 30, belt: "Brown" },
+      { name: "Ken", age: 35, belt: "Orange" },
+    ],
   },
-  methods: {
-    nameToggle() {
-      this.showName = !this.showName;
-    },
-    ageToggle() {
-      this.showAge = !this.showAge;
-    },
-  },
+  methods: {},
 });
